f5f75d99d2cbbfd881b263521a94bf058010b242
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-28 00:43:28 +0900

    chore(flake): remove byteCompileInitEl

--- nix/home.nix
+++ nix/home.nix
@@ -4,11 +4,4 @@
     "~/config/emacs/assets/banner.txt".source = ../assets/banner.txt;
     "~/config/emacs/snippets".source = ../snippets;
   };
-
-  home.activation = {  
-    byteCompileInitEl = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      cd ${config.home.homeDirectory}/${config.programs.emacs-twist.directory}
-      ${config.programs.emacs-twist.config}/bin/emacs --batch -f batch-byte-compile init.el  
-    '';  
-  }; 
 }
