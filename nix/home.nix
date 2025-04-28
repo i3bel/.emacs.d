617a7b294cd2fbee5e7f7ea7977d7353d780c774
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-28 18:17:28 +0900

    chore: modify for byte-compile

--- nix/home.nix
+++ nix/home.nix
@@ -3,4 +3,11 @@
     ".config/emacs/assets/".source = ../assets;
     ".config/emacs/snippets".source = ../snippets;
   };
+
+  home.activation = {  
+    byteCompileInitEl = lib.hm.dag.entryAfter ["writeBoundary"] ''  
+      cd ${config.home.homeDirectory}/${config.programs.emacs-twist.directory}  
+      ${config.programs.emacs-twist.config}/bin/emacs --batch -f batch-byte-compile init.el  
+    '';  
+  }; 
 }
