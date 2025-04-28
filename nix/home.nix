e47f05965145d7d152bec16949c1340db3bfa39f
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-28 18:26:59 +0900

    chore(flake): modify home.activation

--- nix/home.nix
+++ nix/home.nix
@@ -4,10 +4,16 @@
     ".config/emacs/snippets".source = ../snippets;
   };
 
-  home.activation = {  
-    byteCompileInitEl = lib.hm.dag.entryAfter ["writeBoundary"] ''  
-      cd ${config.home.homeDirectory}/${config.programs.emacs-twist.directory}  
-      ${config.programs.emacs-twist.config}/bin/emacs --batch -f batch-byte-compile init.el  
-    '';  
-  }; 
+  home.activation = {
+    byteCompileInitEl = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    cd ${config.home.homeDirectory}/${config.programs.emacs-twist.directory}
+    ${config.programs.emacs-twist.config}/bin/emacs --batch \
+      --eval "(require 'use-package)" \
+      --eval "(eval-and-compile
+                  (setopt use-package-ensure-function #'(lambda (&rest args) t))
+                  (setopt use-package-always-defer t)
+                  (setopt use-package-compute-statistics t))" \
+      -f batch-byte-compile init.el
+  '';
+  };
 }
