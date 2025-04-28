598f07ace628fc1b121b0a3599c26db40a950188
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-28 15:37:46 +0900

    fix(flake): path

--- nix/home.nix
+++ nix/home.nix
@@ -1,7 +1,7 @@
 { config, lib, ... } : {
   home.file = {
-    "~/config/emacs/assets/banner.png".source = ../assets/banner.png;
-    "~/config/emacs/assets/banner.txt".source = ../assets/banner.txt;
-    "~/config/emacs/snippets".source = ../snippets;
+    ".config/emacs/assets/banner.png".source = ../assets/banner.png;
+    ".config/emacs/assets/banner.txt".source = ../assets/banner.txt;
+    ".config/emacs/snippets".source = ../snippets;
   };
 }
