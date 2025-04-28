5ba4c3374a726a2156c1c5d1f62ceb08c52ae149
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-28 19:26:24 +0900

    chore: remove byte-compile option

--- nix/home.nix
+++ nix/home.nix
@@ -3,12 +3,4 @@
     ".config/emacs/assets/".source = ../assets;
     ".config/emacs/snippets".source = ../snippets;
   };
-
-  home.activation = {
-    byteCompileInitEl = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${config.programs.emacs-twist.config}/bin/emacs --batch \
-      --load ${config.home.homeDirectory}/.config/emacs/init.el \
-      -f batch-byte-compile ${config.home.homeDirectory}/.config/emacs/init.el
-  '';
-  };
 }
