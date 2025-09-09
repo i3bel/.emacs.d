516fd16be7a4b85e2765d50f4b1190b8ff9d59b0
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-09-10 00:09:56 +0900

    refactor(default.nix): simplify earlyInitFile definition

--- default.nix
+++ default.nix
@@ -7,9 +7,7 @@
   initFiles = [
     (pkgs.tangleOrgBabelFile "init.el" ./init.org {})
   ];
-  earlyInitFile = [
-    (pkgs.tangleOrgBabelFile "early-init.el" ./early-init.org {})
-  ];
+  earlyInitFile = (pkgs.tangleOrgBabelFile "early-init.el" ./early-init.org {});
   exportManifest = true;
   extraPackages = [];
   extraRecipeDir = ./recipes;
