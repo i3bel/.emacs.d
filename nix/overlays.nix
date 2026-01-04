32d387db6cd1902cd353708adbe622180475e56a
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2026-01-04 18:13:17 +0900

    feat(nix): use emacs revision from flake.lock and fetch from emacs-mirror for emacs-git

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,10 +1,29 @@
+let
+  lock = builtins.fromJSON (builtins.readFile ../flake.lock);
+  emacsInput = lock.nodes.emacs.locked;
+  emacsOverlaySrc = builtins.fetchTree {
+    type = emacsInput.type;
+    owner = emacsInput.owner;
+    repo = emacsInput.repo;
+    rev = emacsInput.rev;
+    narHash = emacsInput.narHash;
+  };
+  emacsMasterMeta = builtins.fromJSON (
+    builtins.readFile (emacsOverlaySrc + "/repos/emacs/emacs-master.json")
+  );
+  emacsMirrorFetch = (builtins.removeAttrs emacsMasterMeta [ "type" "version" ]) // {
+    url = "https://github.com/emacs-mirror/emacs.git";
+  };
+in
 {
   emacs = (final : prev: {
     emacs = prev.emacs.override {
       withNativeCompilation = false;
     };
-    emacs-git = prev.emacs-git.override {
+    emacs-git = (prev.emacs-git.override {
       withNativeCompilation = false;
-    };
+    }).overrideAttrs (_: {
+      src = prev.fetchgit emacsMirrorFetch;
+    });
   });
 }
